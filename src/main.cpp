06c0b1f62536416f2a252c530c56bb2bc10cc34b
Sungki <youbyme@gmail.com>
2021-04-15 19:34:48 -0400

    Added Vector class

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,67 @@
 #include "PixelRay.h"
 #include <iostream>
+#include <vector>
 
-#define M_PI           3.14159265358979323846
+//#define M_PI           3.14159265358979323846
+//float it = 0;
+class Vector
+{
+public:
+    float x;
+    float y;
 
-float it = 0;
+    void addVector(Vector v)
+    {
+        x += v.x;
+        y += v.y;
+    }
+};
+
+class Boid
+{
+public:
+    Vector position;
+    Vector velocity;
+
+    void Update()
+    {
+        position.addVector(velocity);
+    }
+};
+
+std::vector<Boid> flock;
 
 int init()
 {
+    Boid b;
+    b.position.x = 0;
+    b.position.y = 0;
+    b.velocity.x = 0.1f;
+    b.velocity.y = 0.1f;
+
+    flock.push_back(b);
+
 	return 1;
+}
+
+void Edges(Boid& b)
+{
+    if (b.position.x > 320) b.position.x = 0;
+    else if (b.position.x < 0) b.position.x = 320;
+    if (b.position.y > 200) b.position.y = 0;
+    else if (b.position.y < 0) b.position.y = 200;
 }
 
 void step()
 {
-	canvas_clear(1);
+	canvas_clear(0);
 
-    for (int i = 0; i < 300; i++)
+    flock[0].Update();
+
+    Edges(flock[0]);
+    pixel_draw(flock[0].position.x, flock[0].position.y, 3);
+
+/*    for (int i = 0; i < 300; i++)
     {
         float dst = (i / (300 - 1.0f)) * 90;
         float angle = 2 * M_PI * it * i;
@@ -25,5 +72,5 @@ void step()
         pixel_draw(x + 150, y + 100, 3);
     }
 
-    it = it + 0.00001;
+    it = it + 0.00001;*/
 }
